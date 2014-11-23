f30759690b7ef14d9cb5e32f8925945e6a0669ae
Tommy Chheng <tchheng@yahoo-inc.com>
2014-11-23 13:43:22 -0800

    fix mysql querying

--- snpedia-parser/snpedia_store.py
+++ snpedia-parser/snpedia_store.py
@@ -1,15 +1,37 @@
 import json
+import mysql.connector
 
+from mysql.connector import errorcode
 from snpedia import Snpedia
 
 class SnpediaStore:
     def __init__(self):
         self.snpedia = Snpedia()
-        pass
 
-    def create_db(self):
-        """docstring for create_db"""
-        pass
+        self.config = {
+          'user': 'bigfootgenes',
+          'password': 'dk34DFko99FDOQ',
+          'host': '127.0.0.1',
+          'database': 'bigfootgenes_development',
+          'raise_on_warnings': True
+        }
+
+        # get the mysql cursor
+        self.cnx = mysql.connector.connect(**self.config)
+
+    def query(self, rsid, genotype):
+        """Query snps given a rsid and genotype"""
+        query = ("SELECT rsid, genotype, summary FROM snps "
+                 "WHERE rsid = %s AND genotype = %s")
+
+        cursor = self.cnx.cursor()
+        cursor.execute(query, (rsid, genotype))
+
+        for (rsid, genotype, summary) in cursor:
+            record = {'rsid': rsid, 'genotype': genotype, 'summary': summary}
+            yield record
+
+        cursor.close()
 
     def import_data(self, snp_data_file):
         """snp_data_file is created from \
@@ -24,9 +46,10 @@ class SnpediaStore:
     def create_mysql_insert_stmt_from_snpedia_data_line(self, line):
         """docstring for create_mysql_insert_stmt_from_snpedia"""
 
+        # TODO when we switch the data format to real json, use json.loads instead of eval
         # snp_data = json.loads(line)
-
         snp_data = eval(line)
+
         snp = snp_data['snp']
         wikitext = snp_data['wikitext']
 
@@ -35,14 +58,7 @@ class SnpediaStore:
         mysql_stmt = "";
         for geno in record['geno_records']:
             if len(geno) > 0:
-                # TODO reverse complement the geno if orientation is negative
-                #record['orientation']
-
                 stmt = "INSERT INTO snps(rsid,genotype,summary) VALUES(\"{0}\",\"{1}\",\"{2}\");".format(snp, geno['Geno'], geno['Summary'])
                 mysql_stmt += stmt;
 
         return mysql_stmt
-
-    def query(self, rsid, genotype):
-        """docstring for query"""
-        pass
--- snpedia-parser/test/snpedia_store_test.py
+++ snpedia-parser/test/snpedia_store_test.py
@@ -11,17 +11,23 @@ class TestSnpediaStore(unittest.TestCase):
         self.snpedia_store = SnpediaStore()
 
     def test_create_mysql_insert_stmt_from_snpedia_data_line(self):
-        line = ""
         test_file_path = os.path.dirname(os.path.realpath(__file__)) + "/../../data/rs7412.py"
 
+        line = ""
         with open(test_file_path, "r") as f:
             line = f.read()
 
         insert_stmt = self.snpedia_store.create_mysql_insert_stmt_from_snpedia_data_line(line)
+        expected = """INSERT INTO snps(rsid,genotype,summary) VALUES("rs7412","CC","more likely to gain weight if taking olanzapine");INSERT INTO snps(rsid,genotype,summary) VALUES("rs7412","CT","more likely to gain weight if taking olanzapine");INSERT INTO snps(rsid,genotype,summary) VALUES("rs7412","TT","normal");"""
+        self.assertEqual(expected,insert_stmt)
 
-        print insert_stmt
+    def test_query(self):
+        """docstring for test_queyr"""
+        rsid = "rs7412"
+        genotype = "CC"
+
+        for result in self.snpedia_store.query(rsid, genotype):
+            self.assertEqual(result['genotype'], 'CC')
 
 if __name__ == '__main__':
     unittest.main()
-
-
